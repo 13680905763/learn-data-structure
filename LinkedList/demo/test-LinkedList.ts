5308cac5e33493adec6f3a401cd79860db37cd08
13680905763 <177748749@qq.com>
2023-11-07 15:30:10 +0800

    update linkedList end

--- LinkedList/demo/test-LinkedList.ts
+++ LinkedList/demo/test-LinkedList.ts
@@ -5,3 +5,6 @@ linkedList.append('AFA')
 linkedList.append('GG')
 linkedList.append('JDS')
 linkedList.traverse()
+
+// leetcode
+// https://leetcode.cn/problems/delete-node-in-a-linked-list/
--- LinkedList/index.ts
+++ LinkedList/index.ts
@@ -13,6 +13,15 @@ class LinkedList<T> {
     return this.size
   }
 
+  private getNode(position: number): Node<T> | null {
+    let index = 0
+    let current = this.head
+    while (index++ < position && current) {
+      current = current.next
+    }
+    return current
+  }
+
   // 追加节点
   append(value: T) {
     const newNode = new Node(value)
@@ -40,30 +49,77 @@ class LinkedList<T> {
       values.push(current.value)
       current = current.next
     }
-
     console.log(values.join('-->'))
   }
-
+  // 插入
   insert(value: T, position: number) {
     // 越界判断
     if (position < 0 || position > this.size) return false
 
     const newNode = new Node(value)
-
     // 判断是否插入头部
     if (position === 0) {
       newNode.next = this.head
       this.head = newNode
     } else {
-      let current = this.head
-      let previous: Node<T> | null = null
-      let index = 0
-      while (index++ < position && current) {
-        previous = current
-        current = current.next
-      }
+      const previous = this.getNode(position - 1)
+      newNode.next = previous!.next
+      previous!.next = newNode
     }
     this.size++
+  }
+
+  // 根据位置删除
+  removeAt(position: number): T | null {
+    // 越界判断
+    if (position < 0 || position >= this.size) return null
+
+    let current = this.head
+    if (position === 0) {
+      this.head = current?.next ?? null
+    } else {
+      const previous = this.getNode(position - 1)
+      previous!.next = previous?.next?.next ?? null
+      this.size--
+    }
+    return current?.value ?? null
+  }
+
+  // 删除值
+  remove(value: T): T | null {
+    const index = this.indexOf(value)
+    return this.removeAt(index)
+  }
+
+  // 获取
+  get(position: number): T | null {
+    if (position < 0 || position >= this.size) return null
+
+    return this.getNode(position)?.value ?? null
+  }
+  // 更新
+  update(value: T, position: number): boolean {
+    if (position < 0 && position >= this.size) return false
+    const current = this.getNode(position)
+    current!.value = value
+    return true
+  }
+  // 根据值 获取索引
+  indexOf(value: T): number {
+    let current = this.head
+    let index = 0
+    while (current) {
+      if (current.value === value) {
+        return index
+      }
+      current = current.next
+      index++
+    }
+    return -1
+  }
+
+  get isEmpty(): boolean {
+    return this.size === 0
   }
 }
 
